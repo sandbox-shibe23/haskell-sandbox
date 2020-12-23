84f841c9118aafeefb504fb4fa8bf2c7b6aa4208
shibe23 <shibe23.info@gmail.com>
2020-12-24 00:04:58 +0900

    fix Basic01.hs

--- src/Basic01.hs
+++ src/Basic01.hs
@@ -1,8 +1,8 @@
 module Basic01 where
 
 -- 【問1】任意のn番目のフィボナッチ数を計算する関数fibをパターンマッチで実装してください。最初の0は0番目とします。
-fib 1 = 0 + 1
-fib 2 = 0 + 1
+fib 0 = 0
+fib 1 = 1
 fib n = fib (n - 1) + fib (n - 2)
 
 main = do
