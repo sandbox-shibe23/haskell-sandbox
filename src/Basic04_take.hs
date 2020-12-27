3fc8e75400fbd27a616595d407b17755a997d582
shibe23 <shibe23.info@gmail.com>
2020-12-27 23:19:06 +0900

    Add Basic04_take.hs

---
+++ src/Basic04_take.hs
@@ -0,0 +1,9 @@
+module Basic04 where
+
+-- 【問4】sum, product, take, drop, reverseと同じ機能の関数を再実装してください。関数名には'を付けてください。
+take' 0 x = x
+take' 1 (x:xs) = [x]
+take' n (x:xs) = [x] ++ take' ( n-1 ) xs
+
+main = do
+  print $ take' 4 [1..5]
