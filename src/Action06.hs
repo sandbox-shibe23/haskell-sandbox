f827e7d9fb542def7d76d57c82322e8f56f722af
shibe23 <shibe23.info@gmail.com>
2021-01-16 23:28:19 +0900

    Fix Action06.hs

--- src/Action06.hs
+++ src/Action06.hs
@@ -4,7 +4,7 @@ module Action06 where
 fib 0 = return 0
 fib 1 = return 1
 fib n | n > 1 = do
-    return (+) <$> fib n - 2 <*> fib n - 1
+    (+) <$> fib (n - 2) <*> fib (n - 1)
 
 main = do
     print =<< fib 6
