117ee185d9bada6944330d1ac5f069e5161f3447
shibe23 <shibe23.info@gmail.com>
2021-01-16 23:25:46 +0900

    Add Action06.hs

---
+++ src/Action06.hs
@@ -0,0 +1,10 @@
+module Action06 where
+
+-- 【問6】Applicativeスタイルを使って、次のコードから<-を排除してください。
+fib 0 = return 0
+fib 1 = return 1
+fib n | n > 1 = do
+    return (+) <$> fib n - 2 <*> fib n - 1
+
+main = do
+    print =<< fib 6
